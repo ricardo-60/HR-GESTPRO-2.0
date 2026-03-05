e3ce86088ef4dfb192e7d4efa5cee89925893ca7
ricardo-60 <ricardo-60@users.noreply.github.com>
2026-03-05 20:08:31 +0100

    fix: correctly escaped all backticks in ManualBase.ts

--- lib/ManualBase.ts
+++ lib/ManualBase.ts
@@ -67,7 +67,7 @@ O HR-GESTPRO 2.0 gera documentos certificados por algoritmos de assinatura digit
 ### Exportação de SAFT-AO:
 1. Aceda a **Relatórios > Exportar SAFT**.
 2. Selecione o mês pretendido.
-3. O sistema gerará um ficheiro `.xml` validado, pronto para ser submetido no portal da AGT.
+3. O sistema gerará um ficheiro \`.xml\` validado, pronto para ser submetido no portal da AGT.
 
 ---
 
